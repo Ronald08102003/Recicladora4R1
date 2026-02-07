4a7a3a71173afa174acb6a1f6a57969e014f934f
Ronald08102003 <160361535+Ronald08102003@users.noreply.github.com>
2026-02-06 20:43:30 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Connection string de Supabase/Postgres
-  ssl: { rejectUnauthorized: false }         // obligatorio en Render
+  connectionString: process.env.DATABASE_URL, // La URL real de Postgres/Supabase
+  ssl: { rejectUnauthorized: false }         // Necesario en Render
 });
 
 pool.connect(err => {
   if (err) {
-    console.error('❌ Error de conexión:', err.stack);
+    console.error('❌ Error de conexión a la DB:', err.stack);
   } else {
     console.log('✅ Conexión exitosa a la base de datos de Recicladora 4R');
   }
 });
 
 module.exports = pool;
+
+
 
